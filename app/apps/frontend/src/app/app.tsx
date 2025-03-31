daab5656ef3b318857d60e801aff3a23713138eb
JuanPorta14 <juanporta.c@gmail.com>
2025-03-31 18:45:23 -0300

    empezando parte frontend

--- app/apps/frontend/src/app/app.tsx
+++ app/apps/frontend/src/app/app.tsx
@@ -1,12 +1,15 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
 import { Abm } from '../components/abm/abm'
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 export function App() {
   return (
-    <div>
-      <Abm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/abm' element={<Abm />}/>
+      </Routes>
+    </Router>
+
   );
 }
 
--- app/apps/frontend/src/components/abm/abm.tsx
+++ app/apps/frontend/src/components/abm/abm.tsx
@@ -1,10 +1,12 @@
 import styles from './abm.module.css';
-
+import { Button } from '@mantine/core';
 export function Abm() {
   return (
     <div className={styles.mainBox}>
       <div className={styles.interactionBox}>
-
+        <div className={styles.buttonBox}>
+          <Button variant="filled" color="rgba(158, 32, 150, 1)">Button</Button>; 
+        </div>
       </div>
     </div>
   );
