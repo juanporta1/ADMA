5f0bb367744db0d101e9cddbbf75e186a2257a48
JuanPorta14 <juanporta.c@gmail.com>
2025-04-07 21:54:26 -0300

    sacando env

--- app/apps/backend/src/assets/datasource/datasource.module.ts
+++ app/apps/backend/src/assets/datasource/datasource.module.ts
@@ -16,14 +16,9 @@ dotenv.config()
                 try{
                     const dataSource = new DataSource({
                         type:"postgres",
-                        username: process.env.DB_USER,
-                        password:process.env.DB_PASS,
-                        host: process.env.DB_HOST,
-                        database: process.env.DB_NAME,
-                        port:5432,
+                        url: process.env.DB_HOST,
                         entities:[Appoinment],
                         synchronize: true,
-                        ssl:{rejectUnauthorized: false }
                     });
                     await dataSource.initialize();
                     console.log("DataSource initialized successfully");
