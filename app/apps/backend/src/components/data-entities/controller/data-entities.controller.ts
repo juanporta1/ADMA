817880f7919d54fde95c0ffe1c812ba192af86a7
JuanPorta14 <juanporta.c@gmail.com>
2025-04-21 20:06:45 -0300

    sigo cambiando a tablas

--- app/apps/backend/src/components/data-entities/controller/data-entities.controller.ts
+++ app/apps/backend/src/components/data-entities/controller/data-entities.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { DataEntitiesService } from '../services/data-entities.service';
 
 @Controller('data-entities')
@@ -8,5 +8,33 @@ export class DataEntitiesController {
     @Get("specie")
     async getSpecies(){
         return await this.dataService.getSpecies();
+    }
+
+    @Post("specie")
+    async createSpecie(@Body() body: {specie: string}){
+        console.log(body);
+        return await this.dataService.createSpecie(body)
+    }
+
+    @Get("neighborhood")
+    async getNeighborhoods(){
+        return await this.dataService.getNeighborhoods();
+    }
+
+    @Post("neighborhood")
+    async createNeighborhood(@Body() body: {neighborhood: string}){
+        console.log(body);
+        return await this.dataService.createNeighborhood(body)
+    }
+
+    @Get("reason")
+    async getReasons(){
+        return await this.dataService.getResons();
+    }
+
+    @Post("reason")
+    async createReason(@Body() body: {reason: string}){
+        console.log(body);
+        return await this.dataService.createReason(body)
     }
 }
--- app/apps/backend/src/components/data-entities/services/data-entities.service.ts
+++ app/apps/backend/src/components/data-entities/services/data-entities.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Neighborhood } from '../entities/neighborhood.entity';
+import { Specie } from '../entities/specie.entity';
+import { Reason } from '../entities/reason.entity';
+import { privateDecrypt } from 'crypto';
 
 @Injectable()
 export class DataEntitiesService {
+  constructor(
+    @InjectRepository(Neighborhood)
+    private neighborhoodRepository: Repository<Neighborhood>,
+    @InjectRepository(Specie)
+    private specieRepository: Repository<Specie>,
+    @InjectRepository(Reason)
+    private reasonRepository: Repository<Reason>
+  ) {}
 
-    async getSpecies(){
-        
-    }
+  async createSpecie(body: {specie: string}){
+    const newSpecie = this.specieRepository.create(body);
+    return await this.specieRepository.save(newSpecie);
+  }
+
+  async createReason(body: {reason: string}){
+    const newReason = this.reasonRepository.create(body);
+    return await this.reasonRepository.save(newReason);
+  }
+
+  async createNeighborhood(body: {neighborhood: string}){
+    const newNeighborhood = this.neighborhoodRepository.create(body);
+    return await this.neighborhoodRepository.save(newNeighborhood);
+  }
+
+  async createNeighborhoodsBulk(body: {neighborhoods: {neighborhood: string}[]}){
+    const newNeighborhoods = body.neighborhoods.map((neighborhood) => {
+      return this.neighborhoodRepository.create(neighborhood);
+    })
+    return await this.neighborhoodRepository.save(newNeighborhoods);
+  }
+
+  async getSpecies() {}
+
+  async getNeighborhoods() {}
+
+
+  async getReasons() {}
+
 }
